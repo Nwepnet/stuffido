c0bbe883f42e89c2dc20ccc2361a879f142772ee
Nwepnet <149279779+Nwepnet@users.noreply.github.com>
2025-04-14 17:41:38 -0300

    Create sigmastuff.lua

---
+++ sigmastuff.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nwepnet/Skibidi/refs/heads/main/kitkatto.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nwepnet/Skibidi/refs/heads/main/Protected_8306868616713836.txt",true))()
